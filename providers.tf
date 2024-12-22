b37e8a6b5b20b7670b2cc8b56b36869360b463b3
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2024-12-22 01:50:58 -0500

    Removed backend

--- providers.tf
+++ providers.tf
@@ -9,14 +9,6 @@ terraform {
       version = "3.0.2"
     }
   }
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "cloudtherapy"
-
-    workspaces {
-      name = "tf-packer-devops-agent"
-    }
-  }
 }
 
 # Azure Active Directory provider
